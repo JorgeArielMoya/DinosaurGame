4cebbb006992cd7a78218ba36f902d866fc97a56
jorge <jorge@LAPTOP-D8GDLQRL>
2026-07-26 20:29:39 -0400

    Refactor: Refactoriza el codigo del controlador

---
+++ src/controllers/DinoController.java
@@ -0,0 +1,74 @@
+package controllers;
+
+import services.GameEngine;
+import services.DinoManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+
+public class DinoController {
+    @FXML private Pane panelJuego;
+    @FXML private Pane mundoLayer;
+    @FXML private ImageView dinoEstatico;
+    @FXML private Line lineaRecorrido;
+    @FXML private Button btnIniciar;
+    @FXML private Label lblScore;
+
+    private final double GROUND_LEVEL = 230.0;
+
+    private GameEngine gameEngine;
+    private DinoManager dinoManager;
+
+    public void initialize() {
+        panelJuego.setFocusTraversable(true);
+        panelJuego.setOnKeyPressed(this::handleKeyPress);
+        panelJuego.setOnKeyReleased(this::handleKeyRelease);
+
+        lineaRecorrido.setVisible(false);
+
+        dinoManager = new DinoManager(dinoEstatico, GROUND_LEVEL);
+        gameEngine = new GameEngine(mundoLayer, dinoEstatico, GROUND_LEVEL);
+
+        gameEngine.setOnScoreUpdate(score -> lblScore.setText("Score: " + score));
+        gameEngine.setOnGameOver(this::manejarGameOver);
+    }
+
+    @FXML
+    public void iniciarJuego() {
+        btnIniciar.setVisible(false);
+        lineaRecorrido.setVisible(true);
+        mundoLayer.setLayoutX(0);
+
+        lblScore.setText("Score: 0");
+        gameEngine.limpiarListaObstaculos();
+        dinoManager.reiniciarPosicion();
+        
+        gameEngine.iniciar(3.5);
+        panelJuego.requestFocus();
+    }
+
+    private void manejarGameOver() {
+        dinoManager.detenerAnimaciones();
+        btnIniciar.setText("Reintentar");
+        btnIniciar.setVisible(true);
+    }
+
+    private void handleKeyPress(KeyEvent event) {
+        if (event.getCode() == KeyCode.SPACE || event.getCode() == KeyCode.UP) {
+            dinoManager.saltar();
+        } else if (event.getCode() == KeyCode.DOWN || event.getCode() == KeyCode.S) {
+            dinoManager.agacharse();
+        }
+    }
+
+    private void handleKeyRelease(KeyEvent event) {
+        if (event.getCode() == KeyCode.DOWN || event.getCode() == KeyCode.S) {
+            dinoManager.dejarAgacharse();
+        }
+    }
+}
